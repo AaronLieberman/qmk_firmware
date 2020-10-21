e25050e483c6dc9ff896e5366c9025b97fbd3930
Aaron Lieberman <aaronlcode@gmail.com>
2020-10-20 20:07:55 -0700

    updates to match wiring diagram

--- keyboards/handwired/adlkey/keymaps/aaronlieberman/keymap.c
+++ keyboards/handwired/adlkey/keymaps/aaronlieberman/keymap.c
@@ -17,7 +17,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 		KC_Y,       KC_U,       KC_I,       KC_O,       KC_P,       KC_LBRC,    KC_RBRC,    KC_BSLS,                KC_END,
 		KC_H,       KC_J,       KC_K,       KC_L,       KC_SCLN,    KC_QUOT,    KC_ENT,                             KC_PGUP,
 		KC_N,       KC_M,       KC_COMM,    KC_DOT,     KC_SLSH,    KC_RSFT,                            KC_UP,      KC_PGDN,
-		KC_SPC,     KC_LALT,    KC_RGUI,    KC_LCTL,                                        KC_LEFT,    KC_DOWN,    KC_RGHT),
+		KC_SPC,     KC_RALT,    KC_MENU,    KC_RCTL,                                        KC_LEFT,    KC_DOWN,    KC_RGHT),
 
 	KEYMAP(
 		_______,    _______,    _______,    _______,    _______,    _______,    _______,
